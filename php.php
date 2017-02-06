d0cc134ea9e39277605bcd848bf832e319df2604
NalyLas <nataliaramirezlas@gmail.com>
2017-02-06 18:16:21 +0100

    Add files via upload

---
+++ php.php
@@ -0,0 +1,41 @@
+﻿<?php
+$server = "localhost";
+$user = "root";
+$pass = "clave";
+$bd = "BDNatalia";
+
+//Creamos la conexión
+$conexion = mysqli_connect($server, $user, $pass,$bd)
+or die("Ha sucedido un error inexperado en la conexion de la base de datos");
+
+//generamos la consulta
+$sql = $_REQUEST['ins_sql']; 
+mysqli_set_charset($conexion, "utf8"); //formato de datos utf8
+
+if(!$result = mysqli_query($conexion, $sql)) die();
+
+$arraydatos = array(); //creamos un array
+
+while($row = mysqli_fetch_array($result))
+{
+    foreach($row as $key => $value)    
+	{    
+		$arr[$key] = $value; 
+	}    
+	$arraydatos[] = $arr; 
+    
+}
+//desconectamos la base de datos
+$close = mysqli_close($conexion)
+or die("Ha sucedido un error inexperado en la desconexion de la base de datos");
+
+//Creamos el JSON
+$json_string = json_encode($arraydatos);
+echo "".$json_string."";
+
+//Si queremos crear un archivo json, sería de esta forma:
+/*
+$file = 'clientes.json';
+file_put_contents($file, $json_string);
+*/
+?>
